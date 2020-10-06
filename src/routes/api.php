7a015c7fc880e6a6a0bb7c001c2b7b7dfe6d79ee
bitcoder <me@iamir.net>
2020-10-06 10:58:19 +0330

    Update api.php

--- src/routes/api.php
+++ src/routes/api.php
@@ -8,5 +8,5 @@
  */
 
 Route::namespace('v1')->prefix('v1')->middleware('auth:api')->group(function () {
-    Route::apiResource('locations', 'ILogController', ['as' => 'api','except' => ['store','update','destroy']]);
+    Route::apiResource('locations', 'ILocationController', ['as' => 'api','except' => ['store','update','destroy']]);
 });
